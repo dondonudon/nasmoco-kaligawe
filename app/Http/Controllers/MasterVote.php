8c6c077a02f68059f060313a880b15a16e3fb3cd
LaurentiusKevin <laurentiuskevin44@gmail.com>
2019-07-15 07:46:29 +0700

    Master data selesai

--- app/Http/Controllers/MasterVote.php
+++ app/Http/Controllers/MasterVote.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\msVote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
@@ -23,5 +24,48 @@ class MasterVote extends Controller
         } else {
             return abort('403');
         }
+    }
+
+    public function list() {
+        $result['data'] = msVote::all();
+
+        return json_encode($result);
+    }
+
+    public function add(Request $request) {
+        $nama = $request->nama;
+
+        $vote = new msVote;
+        $vote->nama = $nama;
+
+        if ($vote->save()) {
+            $result = [
+                'status' => 'success'
+            ];
+        } else {
+            $result = [
+                'status' => 'failed'
+            ];
+        }
+        return json_encode($result);
+    }
+
+    public function edit(Request $request) {
+        $id = $request->id;
+        $nama = $request->nama;
+
+        $vote = DB::table('ms_vote')->where('id','=',$id);
+
+
+        if ($vote->update(['nama' => $nama])) {
+            $result = [
+                'status' => 'success'
+            ];
+        } else {
+            $result = [
+                'status' => 'failed'
+            ];
+        }
+        return json_encode($result);
     }
 }
--- resources/views/dashboard-master-vote.blade.php
+++ resources/views/dashboard-master-vote.blade.php
@@ -1,5 +1,6 @@
 @extends('dashboard-layout')
 
+
 @section('content')
     <!-- Begin Page Content -->
     <div class="container-fluid">
@@ -27,19 +28,15 @@
                         <table class="table table-hover table-bordered table-sm display nowrap" id="datatable" width="100%">
                             <thead class="text-white bg-primary">
                             <tr>
-                                <th>Username</th>
-                                <th>Nama Lengkap</th>
+                                <th>Nama Vote</th>
                             </tr>
                             </thead>
                         </table>
                     </div>
                     <!-- Card Footer -->
                     <div class="card-footer">
-                        <div class="row">
-                            <div class="col-xl-8"></div>
-                            <div class="col-xl-2">
-                                <button class="btn btn-block btn-outline-danger" id="btnDisable" disabled>Disable</button>
-                            </div>
+                        <div class="row mt-2">
+                            <div class="col-xl-10"></div>
                             <div class="col-xl-2">
                                 <button class="btn btn-block btn-danger" id="btnEdit" disabled>Edit</button>
                             </div>
@@ -49,50 +46,24 @@
             </div>
         </div>
 
-        <div class="row d-none" id="cardData">
+        <div class="row d-none" id="cardComponent">
 
             <div class="col-xl col-lg">
                 <div class="card shadow mb-4">
                     <!-- Card Header - Dropdown -->
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary" id="judulCard">New User</h6>
+                        <h6 class="m-0 font-weight-bold text-primary" id="cardTitle">Opsi Vote Baru</h6>
                     </div>
                     <form id="cardForm">
                     @csrf
                     <!-- Card Body -->
                         <div class="card-body">
-                            <input type="hidden" id="option" value="new">
+                            <input type="hidden" id="cardOption" value="new">
+                            <input type="text" class="d-none" id="inputId" name="id">
                             <div class="form-group">
-                                <label for="inputUsername">Username</label>
-                                <input type="text" class="form-control" id="inputUsername" name="username" placeholder="Username" autocomplete="off" required>
+                                <label for="inputUsername">Nama Opsi Vote</label>
+                                <input type="text" class="form-control" id="inputNama" name="nama" placeholder="Nama Opsi Vote" autocomplete="off" required>
                             </div>
-                            <div class="form-group">
-                                <label for="inputPassword">Password</label>
-                                <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password" autocomplete="off" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputNamaLengkap">Nama Lengkap</label>
-                                <input type="text" class="form-control" id="inputNamaLengkap" name="nama_lengkap" placeholder="Nama Lengkap" autocomplete="off" required>
-                            </div>
-                            <hr style="border-width: 10px;">
-                            @foreach($sidebar as $s)
-                                <div class="form-group row">
-                                    <div class="col-sm-2">{{ $s['group']['nama'] }}</div>
-                                    <div class="col-sm-10">
-                                        <div class="row">
-                                            @foreach($s['menu'] as $m)
-                                                <div class="col-sm-4">
-                                                    <div class="form-check">
-                                                        <input class="form-check-input" type="checkbox" id="permission_{{ $m['id'] }}" name="menu_permission[]" value="{{ $m['id'] }}">
-                                                        <label class="form-check-label" for="permission_{{ $m['id'] }}">{{ $m['nama'] }}</label>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr>
-                            @endforeach
 
                         </div>
                         <!-- Card Footer -->
@@ -113,4 +84,143 @@
         </div>
 
     </div>
+@endsection
+
+@section('script')
+    <script>
+        const cardComponent = $('#cardComponent');
+        const cardForm = $('#cardForm');
+        const cardTitle = $('#cardTitle');
+        const cardOption = $('#cardOption');
+
+        const btnNew = $('#btnNew');
+        const btnEdit = $('#btnEdit');
+        const btnCancel = $('#btnCancel');
+
+        const iId = $('#inputId');
+        const iNama = $('#inputNama');
+
+        let idVote, namaVote;
+
+        const tables = $('#datatable').DataTable({
+            "scrollY": "150px",
+            "scrollX": true,
+            "scrollCollapse": true,
+            // "paging": false,
+            "pageLength": 25,
+            "bInfo": false,
+            "ajax": {
+                "method": "GET",
+                "url": "{{ url('/dashboard/master/vote/list') }}",
+                "header": {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr('content'),
+                }
+            },
+            "columns": [
+                { "data": "nama" },
+            ],
+            "order": [[0,'asc']]
+        });
+
+        $('#datatable tbody').on( 'click', 'tr', function () {
+            let data = tables.row( this ).data();
+            idVote = data.id;
+            namaVote = data.nama;
+            // console.log(idVote);
+            if ( $(this).hasClass('selected') ) {
+                $(this).removeClass('selected');
+                btnEdit.attr('disabled','true');
+            } else {
+                tables.$('tr.selected').removeClass('selected');
+                $(this).addClass('selected');
+                btnEdit.removeAttr('disabled');
+            }
+        });
+
+        function resetForm() {
+            iId.val('');
+            iNama.val('');
+            tables.ajax.reload();
+        }
+
+        $(document).ready(function () {
+            btnNew.click(function (e) {
+                e.preventDefault();
+                resetForm();
+                cardComponent.removeClass('d-none');
+                cardOption.val('new');
+                cardTitle.html('Opsi Vote Baru');
+                $('html, body').animate({
+                    scrollTop: cardComponent.offset().top
+                }, 500);
+            });
+
+            btnEdit.click(function (e) {
+                e.preventDefault();
+                resetForm();
+                cardComponent.removeClass('d-none');
+                cardOption.val('edit');
+                cardTitle.html('Edit Vote');
+
+                iId.val(idVote);
+                iNama.val(namaVote);
+
+                $('html, body').animate({
+                    scrollTop: cardComponent.offset().top
+                }, 500);
+            });
+
+            btnCancel.click(function (e) {
+                e.preventDefault();
+                $("html, body").animate({ scrollTop: 0 }, 500, function () {
+                    cardComponent.addClass('d-none');
+                    resetForm();
+                });
+            });
+
+            cardForm.submit(function (e) {
+                e.preventDefault();
+                let url;
+                if (cardOption.val() == 'new') {
+                    url = "{{ url('dashboard/master/vote/add') }}";
+                } else {
+                    url = "{{ url('dashboard/master/vote/edit') }}";
+                }
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    }
+                });
+                $.ajax({
+                    url: url,
+                    method: "post",
+                    data: $(this).serialize(),
+                    success: function(result) {
+                        // console.log(result);
+                        var data = JSON.parse(result);
+                        if (data.status == 'success') {
+                            Swal.fire({
+                                type: 'success',
+                                title: 'Berhasil',
+                                text: 'Data tersimpan',
+                                onClose: function() {
+                                    $("html, body").animate({ scrollTop: 0 }, 500, function () {
+                                        cardComponent.addClass('d-none');
+                                        resetForm();
+                                        tables.ajax.reload();
+                                    });
+                                }
+                            });
+                        } else {
+                            Swal.fire({
+                                type: 'info',
+                                title: 'Gagal',
+                                text: data.reason,
+                            });
+                        }
+                    }
+                });
+            })
+        })
+    </script>
 @endsection
